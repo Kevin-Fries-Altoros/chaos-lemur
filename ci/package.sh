a3295ed518091196b5b980bbbd9cc23e6e55bc51
Kevin Fries <kevin.fries@altoros.com>
2016-12-22 11:05:02 -0800

    Added pipeline elements

--- ci/package.sh
+++ ci/package.sh
@@ -2,9 +2,33 @@
 
 set -e
 
+parse_yaml() {
+   local prefix=$2
+   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   sed -ne "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+   awk -F$fs '{
+      indent = length($1)/2;
+      vname[indent] = $2;
+      for (i in vname) {if (i > indent) {delete vname[i]}}
+      if (length($3) > 0) {
+         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+      }
+   }'
+}
+
 cd chaos-lemur
+
+if [ -f ./ci/env.yml ]; then
+  eval $(parse_yaml ./ci/env.yml "env_")
+fi
+
 ./mvnw clean package
 
 cp manifest.* ../chaos-lemur-deploy
-cp -r target ../chaos-lemur-deploy
+if [ -n "${env_env}"]; then
+  echo "${env_env}" >> ../chaos-lemur-deploy/manifest.yml
+fi
 
+cp -r target ../chaos-lemur-deploy
