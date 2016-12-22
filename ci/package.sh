bf7670e165b03b51f4d9c5e295ee9dee055c09b5
Kevin Fries <kevin.fries@altoros.com>
2016-12-22 13:32:04 -0800

    Added pipeline elements

--- ci/package.sh
+++ ci/package.sh
@@ -23,6 +23,6 @@ cd chaos-lemur
 ./mvnw clean package
 
 cp manifest.* ../chaos-lemur-deploy
-if [ -f ci/env.yml ]; then cat ci/env.yml >> ../chaos-lemur-deploy/manifest.yml; fi
+if [ -f ci/env.yml ]; then sed '/^---$/d' ci/env.yml >> ../chaos-lemur-deploy/manifest.yml; fi
 
 cp -r target ../chaos-lemur-deploy
